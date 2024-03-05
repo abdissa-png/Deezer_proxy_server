8cc46dec9700e433b3ca83568bf06b75739e647a
Abdissa-Degefu <116519055+abdissa-png@users.noreply.github.com>
2024-03-05 20:39:43 +0300

    Revert back to deezer api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require('cors'); 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 // Create Express Server
 const dotenv = require("dotenv");
@@ -11,38 +11,32 @@ const corsOptions = {
     origin: 'https://melodify11.netlify.app', // Specify the allowed origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
     allowedHeaders: 'Content-Type,Authorization' // Specify allowed headers
-};
-
+  };
+app.use("*",cors(corsOptions));
 // Logging
 app.use(morgan('dev'));
 // Configuration
 const PORT = process.env.PORT;
-const X_RapidAPI_key=process.env.X_RapidAPI_key;
-const X_RapidAPI_Host=process.env.X_RapidAPI_Host;
-
-const API_SERVICE_URL = "https://deezerdevs-deezer.p.rapidapi.com";
+console.log("PORT: ",PORT)
+const API_SERVICE_URL = "https://api.deezer.com";
 
 // Proxy endpoints
-app.use('/api', cors(corsOptions), createProxyMiddleware({
+app.use('/api', createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
     pathRewrite: {
         [`^/api`]: '',
     },
-    onProxyReq: (proxyReq, req, res) => {
-        // Add the custom headers
-        proxyReq.setHeader('X-RapidAPI-Key', X_RapidAPI_key);
-        proxyReq.setHeader('X-RapidAPI-Host', X_RapidAPI_Host);
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
     }
-}));
-
-// Error handling middleware
-app.use(function (err, req, res, next) {
+ }));
+ app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
-
-// Start the Proxy
+ // Start the Proxy
 app.listen(PORT, () => {
     console.log(`Starting Proxy at Port:${PORT}`);
-});
+ });
+
